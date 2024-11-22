b883c4a258c8ce11807727699931804bc648f2c4
dami0709 <damihen0208@gmail.com>
2024-11-22 14:36:13 +0100

    maj

--- audio_analysis_dashboard.py
+++ audio_analysis_dashboard.py
@@ -14,6 +14,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 import librosa.display
 import os
+import torchvision
 
 class CustomResNet18(nn.Module):
     def __init__(self, num_classes=6):
